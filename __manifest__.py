a58e775f1028bf6989785e071bbf75f75ee4daee
Marti <martic20@gmail.com>
2018-04-12 18:56:35 +0200

    basic views and model

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "a1",
+    'name': "a1-martic20",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Tercer intent de mòdul, aques funcionarà""",
 
     'description': """
-        Long description of module's purpose
+        Descripció curta
     """,
 
-    'author': "My Company",
+    'author': "martic20",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -25,8 +24,9 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/todo_menu.xml',
+        'views/todo_view.xml',
+        #'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
